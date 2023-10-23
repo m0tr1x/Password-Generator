d9c854c720783c72b85feeecf14d73aca236d913
m0tr1x <grigorysyzran@mail.ru>
2023-10-23 21:41:59 +0300

    Made xml grid

--- Code/MainWindow.xaml.cs
+++ Code/MainWindow.xaml.cs
@@ -24,5 +24,8 @@ namespace Code
         {
             InitializeComponent();
         }
+
+       
+      
     }
 }
--- Code/obj/Debug/net6.0-windows/MainWindow.g.cs
+++ Code/obj/Debug/net6.0-windows/MainWindow.g.cs
@@ -53,7 +53,7 @@ namespace Code {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Code;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Code;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
