7f44f8961129945b1c223c3eb9df726da999f4c0
m0tr1x <grigorysyzran@mail.ru>
2023-10-23 22:21:33 +0300

    smth that works

--- Code/Generator.cs
+++ Code/Generator.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace Code
 {
+    /// <summary>
+    /// Генерируем пароль основываясь на его длине и условиях, которые тыкнуты 
+    /// </summary>
     internal class Generator
     {
+        private static string _special = "!#$%&()*+./:;=>?@[\\]^`{|}~'\\";
+        private static string _divider = "_-";
+        private static string _space = " "; 
+        private static string _capitalLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
+        private static string _lowLetters = "abcdefghijklmnopqrstuvwxyz"; 
+        private static string _numbers = "01234567890";
+        private static string[] all = {_special, _divider , _space, _capitalLetters, _lowLetters, _numbers };
+
+        public static string GeneratePass(int length, bool?[] conditions)
+        {
+            Random rnd = new();
+            var chars = new StringBuilder();
+            var result = new StringBuilder();
+            for (int i=0; i<conditions.Length; i++)
+            {
+                if ((bool)conditions[i]) chars.Append(all[i]);
+            }
+           
+            for(int j = 0; j < length; ++j)
+            {
+                result.Append(chars[rnd.Next(chars.Length-1)]);
+            }
+            return result.ToString();
+        }
+
+
+
     }
 }
--- Code/MainWindow.xaml.cs
+++ Code/MainWindow.xaml.cs
@@ -25,7 +25,42 @@ namespace Code
             InitializeComponent();
         }
 
-       
-      
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Заполняем условия для генерации
+            bool?[] conditions = new bool?[6];
+            conditions[0] = Special.IsChecked;
+            conditions[1] = Divider.IsChecked;
+            conditions[2] = Space.IsChecked;
+            conditions[3] = Capital.IsChecked;
+            conditions[4] = Low.IsChecked;
+            conditions[5] = Number.IsChecked;
+
+            // Узнаем количество символов и паролей
+            int passLength = Convert.ToInt32(PassLen.Text);
+            int passNum = Convert.ToInt32(PassNum.Text);
+
+            bool flag = false;
+
+            if (passNum == 0) Result.Text = "Нельзя сгенерировать 0 паролей :(";
+            if (passLength == 0) Result.Text = "Нельзя сгенерировать пароль длинной 0 символов :(";
+            
+            
+            foreach (var cond in conditions)
+            {
+                if ((bool)cond) flag = true;
+            }
+
+            if (flag)
+            {
+                for (int i = 0; i < passNum; ++i)
+                {
+                    Result.Text += Generator.GeneratePass(passLength, conditions);
+                }
+            }
+            else Result.Text = "Нельзя сгенерировать пароль из пустоты :(";
+           
+
+        }
     }
 }
